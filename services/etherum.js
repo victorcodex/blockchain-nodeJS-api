368101333448456e9d743c8224fe99475dfdfb13
VictorCodex <vifeanyi@gmail.com>
2018-10-30 17:17:07 +0100

    default error for unimplemented crypto services

--- services/etherum.js
+++ services/etherum.js
@@ -4,23 +4,23 @@ class etherum{
 	}
 
 	createAccount(){
-		//
+		throw new Error('Sorry. This network is not yet available. Please try back soon')
 	}
 
 	fundWallet(){
-		//
+		throw new Error('Sorry. This network is not yet available. Please try back soon')
 	}
 
 	sendFunds(){
-		//
+		throw new Error('Sorry. This network is not yet available. Please try back soon')
 	}
 
 	balance(){
-		//
+		throw new Error('Sorry. This network is not yet available. Please try back soon')
 	}
 
 	getTransactions(){
-		//
+		throw new Error('Sorry. This network is not yet available. Please try back soon')
 	}
 }
 
